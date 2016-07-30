7eb4591ea392dc3b280577c6de54a6625e922433
Mathias Rav <rav@cs.au.dk>
2016-07-30 15:28:52 +0200

    Work on compile.py

--- compile.py
+++ compile.py
@@ -1,6 +1,8 @@
+import re
 import os
-import sys
+import json
 import argparse
+import functools
 import subprocess
 
 import dukpy
@@ -37,7 +39,7 @@ def compiler(src_ext, dest_ext):
     return decorator
 
 
-def transform_file(input_filename, output_filename, fn):
+def transform_file(input_filename, output_filename, fn, exn_fn):
     prev_source = None
 
     def transformer():
@@ -49,9 +51,16 @@ def transform_file(input_filename, output_filename, fn):
             return
         prev_source = source
         print("Transform %s to %s" % (input_filename, output_filename))
-        result = fn(source)
-        with open(output_filename, 'w') as fp:
-            fp.write(result)
+        try:
+            result = fn(source)
+        except dukpy.JSRuntimeError as exn:
+            if exn_fn:
+                exn_fn(exn)
+            else:
+                print(exn)
+        else:
+            with open(output_filename, 'w') as fp:
+                fp.write(result)
 
     return transformer
 
@@ -59,6 +68,40 @@ def transform_file(input_filename, output_filename, fn):
 @compiler('.es6', '.js')
 def babel_compile(source):
     return dukpy.babel.babel_compile(source)['code']
+
+
+def pass_exception_to_vim(servername, filename):
+    def handle_exception(exn):
+        print(exn)
+        mo = re.search(r'^SyntaxError: .*: (.*) \((\d+):(\d+)\)\n', str(exn))
+        if mo:
+            text = mo.group(1)
+            lnum = mo.group(2)
+            col = mo.group(3)
+            quickfix_entry = {
+                "filename": filename,
+                "lnum": int(lnum),
+                "col": int(col) + 1,
+                "text": text,
+            }
+            cmdline = ['vim', '--servername', servername, '--remote-expr',
+                       '[setqflist([%s]), feedkeys(":\<C-U>cc 1\<CR>")]' %
+                       json.dumps(quickfix_entry)]
+            subprocess.check_call(cmdline)
+
+    return handle_exception
+
+
+def send_ok_to_vim(servername, message, fn):
+    @functools.wraps(fn)
+    def wrapped(*args, **kwargs):
+        res = fn(*args, **kwargs)
+        cmdline = ['vim', '--servername', servername, '--remote-send',
+                   ':<C-U>echom %s<CR>' % json.dumps(message)]
+        subprocess.check_call(cmdline)
+        return res
+
+    return wrapped
 
 
 #@compiler('.js', '.es6')
@@ -73,6 +116,7 @@ def babel_compile(source):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--watch', action='store_true')
+    parser.add_argument('--vim-servername')
     parser.add_argument('filename')
     args = parser.parse_args()
     base, ext = os.path.splitext(args.filename)
@@ -81,7 +125,12 @@ def main():
     except KeyError:
         parser.error("Filename must end with one of: %s" % sorted(compilers))
     output_filename = base + dest_ext
-    transformer = transform_file(args.filename, output_filename, fn)
+    exn_fn = None
+    if args.vim_servername:
+        fn = send_ok_to_vim(
+            args.vim_servername, "Compiled to %s" % output_filename, fn)
+        exn_fn = pass_exception_to_vim(args.vim_servername, args.filename)
+    transformer = transform_file(args.filename, output_filename, fn, exn_fn)
     transformer()
     if args.watch:
         for _ in await_changes(args.filename, ['modify']):
