d8f0c2293318560f9ba04ab0b7891e9669774dbf
Mathias Rav <rav@cs.au.dk>
2016-08-06 14:59:40 +0200

    Report RenderError when pdflatex fails

--- lokaleplan/texrender.py
+++ lokaleplan/texrender.py
@@ -3,22 +3,27 @@ import tempfile
 import subprocess
 
 
+class RenderError(subprocess.CalledProcessError):
+    pass
+
+
 def tex_to_pdf(source):
     with tempfile.TemporaryDirectory() as d:
         jobname = 'plans'
         base = os.path.join(d, jobname)
         with open(base + '.tex', 'w') as fp:
             fp.write(source)
+        cmd = ('pdflatex', base + '.tex')
         p = subprocess.Popen(
-            ('pdflatex', base + '.tex'),
+            cmd,
             cwd=d,
             stdin=subprocess.DEVNULL,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             universal_newlines=True)
         with p:
-            stdout, _ = p.communicate()
+            output, _ = p.communicate()
         if p.returncode != 0:
-            raise Exception("pdflatex failed: %s" % p.returncode)
+            raise RenderError(p.returncode, cmd, output, stderr=None)
         with open(base + '.pdf', 'rb') as fp:
             return fp.read()
